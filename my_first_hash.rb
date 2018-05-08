1360b93617fed4d8aac5daee077bdced405101bc
Grace Shih <vocalise@gmail.com>
2018-05-08 18:44:07 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,10 +37,11 @@ def adding
 
 
   # add 4 gun powder to the shipping_manifest hash below
-  shipping_mainfest["gun powder"] = 4
+  shipping_manifest["gun powder"] = 4
 
 
 
   # return the shipping_manifest hash below
+  shipping_manifest
 
 end

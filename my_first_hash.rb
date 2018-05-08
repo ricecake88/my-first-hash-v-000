954ba48134cf68678075b091c5a4b445943067ca
Grace Shih <vocalise@gmail.com>
2018-05-08 18:43:29 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,7 +21,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  puts shipping_mainfest["oil paintings"]
+  shipping_manifest["oil paintings"]
 
 end
 
